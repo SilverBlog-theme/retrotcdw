c7ed6b82ff7a3b315cde296d22151279fd72e534
tcdw <tcdw2011@gmail.com>
2018-09-19 19:36:42 +0800

    头部完成

--- rollup.config.js
+++ rollup.config.js
@@ -3,8 +3,10 @@ import postcss from 'rollup-plugin-postcss';
 import { uglify } from 'rollup-plugin-uglify';
 import autoprefixer from 'autoprefixer';
 import clean from 'postcss-clean';
+import progress from 'rollup-plugin-progress';
+import replace from 'rollup-plugin-replace';
 
-const isDist = process.env.BUILD === 'production';
+const isDist = process.env.NODE_ENV === 'production';
 
 const buildPlan = {
     input: 'source/js/index.js',
@@ -14,6 +16,12 @@ const buildPlan = {
         sourcemap: !isDist,
     },
     plugins: [
+        progress({
+            clearLine: false,
+        }),
+        replace({
+            'process.env.NODE_ENV': process.env.NODE_ENV,
+        }),
         postcss({
             extract: true,
             sourceMap: !isDist,
---
+++ source/js/index.js
@@ -0,0 +1,4 @@
+// import '../../node_modules/normalize.css/normalize.css';
+import '../scss/index.scss';
+
+console.log('hi');
